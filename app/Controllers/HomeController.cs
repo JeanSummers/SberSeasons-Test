fe1d1f00e3207f29be4bc3ca8971c674012cd180
MistHome <hatersparty@gmail.com>
2018-06-10 22:56:24 +0300

    Minor changes

--- app/Controllers/HomeController.cs
+++ app/Controllers/HomeController.cs
@@ -24,7 +24,8 @@ namespace app.Controllers
         {
             return View();
         }
-    
+
+    // TODO: Automate news posting
         public IActionResult News1()
         {
             return View();
--- app/wwwroot/js/site.js
+++ app/wwwroot/js/site.js
@@ -1,1 +1,1 @@
-﻿// Write your JavaScript code.
+// Placeholder
