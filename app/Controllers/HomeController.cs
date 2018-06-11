9b7cd73a5dbb7962cb1cb3b3ddee5089241100ca
MistHome <hatersparty@gmail.com>
2018-06-11 12:42:45 +0300

    Less code duplication and dynamic views

--- app/Controllers/HomeController.cs
+++ app/Controllers/HomeController.cs
@@ -5,34 +5,64 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using app.Models;
+using sberseasons.Models;
 
 namespace app.Controllers
 {
+
     public class HomeController : Controller
     {
+
+        // Предполагается, что новости будут подгружаться из внешнего источника
+        NewsModel[] NewsList = new NewsModel[]
+        {
+            new NewsModel
+            {
+                number = 0,
+                header = "Сбербанк захотел выйти из-под контроля государства",
+                headerSmall = "Сбербанк захотел выйти<br> из-под контроля государства",
+                mainImg = "/images/pic01.jpg",
+                shortHTML = "Глава Сбербанка Герман Греф призвал<br> " +
+                            "государство продать контрольную долю в крупнейшей<br> " +
+                            "финансовой организации России",
+                fullHTML = "News1"
+            },
+            new NewsModel
+            {
+                number = 1,
+                header = "В работе трейдинговой платформы Сбербанка произошел сбой",
+                headerSmall = "В работе трейдинговой платформы <br>Сбербанка произошел сбой",
+                mainImg = "/images/pic02.jpg",
+                shortHTML = "В работе брокера Сбербанка произошел кратковременный<br> " +
+                            "технический сбой. О проблемах с проведением<br> " +
+                            "операций сообщил РБК клиент банка",
+                fullHTML = "News2"
+            }
+        };
+
+       
+
         public IActionResult Index()
         {
+            ViewBag.News = NewsList;
             return View();
         }
 
         public IActionResult About()
         {
+            // Placeholder
             return View();
         }
 
         public IActionResult Contact()
         {
+            // Placeholder
             return View();
         }
 
-    // TODO: Automate news posting
-        public IActionResult News1()
+        public IActionResult News(int number)
         {
-            return View();
-        }
-
-        public IActionResult News2()
-        {
+            ViewBag.NewsData = NewsList[number];
             return View();
         }
 
---
+++ app/Models/NewsModel.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sberseasons.Models
+{
+    public class NewsModel
+    {
+        public int number;
+        public string header;
+        public string headerSmall;
+        public string mainImg;
+        public string shortHTML;
+        public string fullHTML;
+    }
+}
